2c52b01d6c8ca4c45b00c12519b858a5dcf88f29
Lia Schulze Dephoff <lia.schulze.dephoff@rwth-aachen.de>
2020-06-17 10:55:55 +0200

    finished movie app for android in xamarin

---
+++ MovieApp2/MovieApp2/MovieApp2/DetailView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using unirest_net.http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieApp2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailView : ContentPage, INotifyPropertyChanged
+    {
+        public DetailView(Top250DataDetail content)
+        {
+            InitializeComponent();
+            lblTitle.Text = content.Title;
+            lblCrew.Text = content.Crew;
+            lblYear.Text = content.Year;
+            image.Source = content.Image;
+            lblShortDescr.Text = getWikipediaDataPlotShort(content.Id);
+            lblRating.Text = content.IMDbRating;
+        }
+    
+        private string getWikipediaDataPlotShort(string id)
+        {
+            WikipediaData WikiData;
+            HttpResponse<WikipediaData> request = Unirest.get("https://imdb-api.com/en/API/Wikipedia/k_kosg24pB/"+id)
+            .header("X-RapidAPI-Key", "k_kosg24pB")
+            .asJson<WikipediaData>();
+            WikiData = request.Body;
+            if (request.Body.ErrorMessage != null && request.Body.ErrorMessage != "")
+            {
+                DisplayAlert("Alert", request.Body.ErrorMessage, "OK");
+            }
+            return WikiData.PlotShort.PlainText;
+        }
+
+    }
+
+    public class WikipediaData
+    {
+        public string IMDbId { get; set; }
+        public string Title { get; set; }
+        public string FullTitle { get; set; }
+        public string Type { get; set; }
+        public string Year { get; set; }
+
+        public string Language { get; set; }
+        public string TitleInLanguage { get; set; }
+        public string Url { get; set; }
+
+        public WikipediaDataPlot PlotShort { get; set; }
+        public WikipediaDataPlot PlotFull { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+
+    public class WikipediaDataPlot
+    {
+        public string PlainText { get; set; }
+        public string Html { get; set; }
+    }
+}
---
+++ MovieApp2/MovieApp2/MovieApp2/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using unirest_net.http;
+using Xamarin.Forms;
+
+namespace MovieApp2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public IList<Top250DataDetail> Movies { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            getDataFromServer();
+            
+            BindingContext = this;
+
+            MovieXamlList.RefreshCommand = new Command(() => {
+                getDataFromServer();
+                MovieXamlList.IsRefreshing = false;
+            });
+        }
+
+        private void getDataFromServer()
+        {
+            Movies = new List<Top250DataDetail>();
+            HttpResponse<Top250Data> request = Unirest.get("https://imdb-api.com/en/API/Top250Movies/k_kosg24pB")
+            .header("X-RapidAPI-Key", "k_kosg24pB")
+            .asJson<Top250Data>();
+            for (int i = 0; i < 250; i++)
+            {
+                Top250DataDetail movie = request.Body.Items[i];
+                movie.IMDbRating = getRating(movie);
+                Movies.Add(movie);
+
+            }
+            if (request.Body.ErrorMessage != null && request.Body.ErrorMessage != "" )
+            {
+                DisplayAlert("Alert", request.Body.ErrorMessage, "OK");
+            }
+        }
+
+        private string getRating(Top250DataDetail content)
+        {
+            int emoji = 0x1F3AC;
+            Console.WriteLine(content.IMDbRating);
+            var sb = new StringBuilder(Convert.ToInt32(Double.Parse(content.IMDbRating)));
+            for (int i = 0; i < Convert.ToInt32(Double.Parse(content.IMDbRating)); i++)
+            {
+                sb.Append(Char.ConvertFromUtf32(emoji) + " ");
+            }
+            sb.Append(" (" + content.IMDbRating + ")");
+            return sb.ToString(); ;
+        }
+
+        private void List_view_item_tapped(object sender, ItemTappedEventArgs e)
+        {
+            var content = e.Item as Top250DataDetail;
+            Navigation.PushAsync(new DetailView(content));
+        }
+
+    }
+    public class Top250Data
+    {
+        public List<Top250DataDetail> Items { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+
+    public class Top250DataDetail
+    {
+        public string Id { get; set; }
+        public string Rank { get; set; }
+        public string Title { set; get; }
+        public string FullTitle { set; get; }
+        public string Year { set; get; }
+        public string Image { get; set; }
+        public string Crew { get; set; }
+        public string IMDbRating { get; set; }
+        public string IMDbRatingCount { get; set; }
+    }
+
+}
